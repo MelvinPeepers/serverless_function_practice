c29f77203ca43423210183ba222132d180bf45a2
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2021-11-21 10:08:35 -0500

    Basic setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
+import function_pic from './img/function_pic.png';
 import './App.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const fetchBtn = document.getElementById('fetch-btn')
+  const responseText = document.getElementById('response-output')
+
+  fetchBtn.addEventListener('click', async () => {
+    const response = await fetch('/.netlify/functions/hello').then(
+      response => response.json()
+    )
+
+    responseText.innerText = JSON.stringify(response)
+  })
+})
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.netlify.com/products/functions/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h1>Serverless Functions</h1>
         </a>
+            <ul className="list">
+            <li>Not built for long running processes</li>
+            <li> Primarily Stateless</li>
+            <li>Can have performance or latency implications that can impact specfific types of problems</li>
+            </ul>
+          <h2>Basic anatomy of a serverless function</h2>
+          <img src={function_pic} alt="anatomy of a serverless function" />
+          <button id='fetch-btn'>Fetch</button>
+          <p id="response-output">Response Placeholder</p>
       </header>
     </div>
   );
