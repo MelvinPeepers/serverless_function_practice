4af5e025a275e9b17f38d83d2c14c9c9569bdbb6
Melvin Peepers <peepersm@gmail.com>
2022-02-28 06:14:15 -0500

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,9 +1,9 @@
 exports.handler = async function () {
+    console.log("hello hello")
     return {
         statusCode: 200,
         body: JSON.stringify({
             message: "Hello World!"
         })
-        console.log("hello hello")
     }
 }
